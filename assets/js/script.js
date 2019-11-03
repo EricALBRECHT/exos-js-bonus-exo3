a5a8ded664e57c979086c8cf7a9e4162ee57d388
Eric <e.albrecht@laposte.net>
2019-11-03 23:30:44 +0100

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var tableau = ["Javascript", "PHP", "MySQL", "C-Sharp", "Html", "CSS"];
+
+var index = tableau.indexOf('C-Sharp');
+
+document.getElementById('reponse').innerText = `Dans le tableau contenant: ${tableau}, C-Sharp à l'index n°${index}.`;
